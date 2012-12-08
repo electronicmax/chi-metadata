8132dfd1fd847cfd00b011f8b845f5748cf7550e
electronic max <electronic@gmail.com>
2012-12-08 11:59:24 +0000

    pretty

--- src/getaffils.py
+++ src/getaffils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python 
 
-import csv, sys, re
+import csv, sys, re, json
 from collections import defaultdict
 
 primary_re = re.compile('Primary Affiliation (\d+) - (\S+)$')
@@ -18,15 +18,24 @@ def compile_affils(row, his):
             affils[author][field] = row[col]
     return dict(affils)
 
-def read_file(name='../from-pcs/4-dec-2013/listOfSubmissions (10).csv'):
+def read_file(name):
     ff = csv.reader(open(name,'rU'))
     rows = [x for x in ff]
     trash, trash, headers  = rows[0],rows[1],rows[2]
     submissions = [r for r in rows[3:] if len(r)]
     return [compile_affils(s, header_idxs(headers)) for s in submissions]
 
+def process(name='../from-pcs/4-dec-2013/listOfSubmissions (10).csv'):
+    infile = name
+    outfile = "{0}.out.json".format(infile[:-4])
+    print "writing to outfile - ", outfile
+    out = open(outfile,'w')
+    z = read_file(infile)
+    out.write(json.dumps(z))
+    out.close()
+
 def test():
-    return read_file()
+    return process()
 
 # if __name__ == '__main__':
 #     fname = sys.argv[1]
