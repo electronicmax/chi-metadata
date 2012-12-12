21ab6dfd1ed2b764497ea9ec8ba1508a7883ec9d
electronic max <electronic@gmail.com>
2012-12-12 19:50:24 +0000

    arrghh unicode

--- src/cluster.py
+++ src/cluster.py
@@ -1,5 +1,10 @@
 
-import csv, fnmatch, re, os, json
+import csv, fnmatch, re, os, json, codecs
+
+def decode(s):
+    print s
+    d1 = codecs.decode(s,'iso_8859_1','ignore')
+    return d1.encode(encoding='utf_8')
 
 def touch(fname, times=None):
     with file(fname, 'a'):
@@ -29,14 +34,15 @@ class AffiliationDB:
         filename = os.path.sep.join([DBDIR, 'affils-{0}.csv'.format(val + 1)])
         print "creating new database ", filename
         touch(filename)
-    def __contains__(self, item): return self.get_canonical(item) is not None
+    def __contains__(self, item): return self.get(item) is not None
     def _parse_cell(self,val):  return tuple([x.strip() for x in val.split('|')])
-    def _serial_cell(self,cell): return '|'.join([x.strip() for x in cell])
+    def _serial_cell(self,cell):
+        return '|'.join([x.strip() for x in cell])
     def load(self):
         ## loads the highest-incremented database to disk ##
         data = {}
         filename = os.path.sep.join([DBDIR, self.get_current_db_filename()])
-        db = open(filename,'r')
+        db = codecs.open(filename,'r','utf_8')
         print "opening ", filename
         rows = csv.reader(db)
         for line in rows:
@@ -49,34 +55,36 @@ class AffiliationDB:
         # _overwriting the most recent dictionary_ unless auto
         if make_new: self.create_new_db()
         filename = os.path.sep.join([DBDIR, self.get_current_db_filename()])
-        db = open(filename,'w')
-        writer = csv.writer(db)
+        db = codecs.open(filename,'w','utf_8')
+        writer = csv.writer(db, dialect='excel')
         for canon,row in self._data.iteritems():
-            print canon,row
+            print canon,row,type
             rsers = [self._serial_cell(cell) for cell in row ]
             rcanon = self._serial_cell(canon)
-            writer.writerow([rcanon] + rsers)            
+            try:
+                writer.writerow([rcanon] + rsers)
+            except ValueError as e:
+                print "ERROR --------------------------------------- "
+                pass            
         db.close()
-    def get_row(self,canon): return self._data.get(canon)
+    def get(self,canon): return self._data.get(canon)
     def set_row(self,canon,row): self._data[canon] = row
-    def add(self, canon, new_affil):
+    def add(self, canon, new_affil=None):
         if new_affil == canon: return True
-        if self.get_row(new_affil):
+        if new_affil and canon and self.get(new_affil):
             # means we want to add new affil (and all its affiliations as an affil of canon)
-            return self.merge_rows(canon, new_affil)        
-        row = self.get_row(canon) or []
+            return self.merge_rows(canon, new_affil)
+        # new affil we've never seen before
+        row = self.get(canon) or []
         if new_affil and new_affil not in row:
             self.set_row(canon, row + [new_affil])
         else:
             self.set_row(canon, row)
-    def get_canonical(self, canon):
-        # looks up for an exact match
-        return self._data.get(canon)
-    def merge_rows(self,target_canon,src_canon):
+    def merge_rows(self,target,src_canon):
         ## merges two lines
-        new_row = [src_canon] + (self.get_row(target_canon) or []) + (self.get_row(src_canon) or [])
+        new_row = [src_canon] + (self.get(target) or []) + (self.get(src_canon) or [])
         new_row = list(set(new_row)) # de-duplicate
-        self.set_row(target_canon, new_row)
+        self.set_row(target, new_row)
         del self._data[src_canon]
 ## 
 def match(db, target):
@@ -85,21 +93,27 @@ def match(db, target):
     ## @param target is a 3-tuple ( Institution, City, Country ) 
     pass
 
-def load_json(json_file, db):
-    jdata = json.loads(open(json_file,'r'))
+def load_json(json_file):
+    return json.loads(''.join(codecs.open(json_file,'r','utf_8').readlines()))
+
+def load_into_db(json_file,db):
+    print "loading ", json_file
+    jdata = load_json(json_file)
     def load(authors):
         auth = (author['Institution'], author['City'], author['Country'])
-        if db.get(auth): pass
-        db.
-    [[author for author in paper['primary'].values()] for paper in jdata]
-    [[author for author in paper['2ndary'].values()] for paper in jdata]
-    
+        if db.get(auth):  pass
+        db.add(auth)
+    [[load(author) for author in paper['primary'].values()] for paper in jdata]
+    [[load(author) for author in paper['2ndary'].values()] for paper in jdata]
 
-def load_in(filename):
+def load_entire_dir(dirname):
+    db = AffiliationDB()    
+    names = [load_into_db(os.path.sep.join([dirname,fname]),db) for fname in os.listdir(dirname) if fnmatch.fnmatch(fname,'*.out.json')]
+    db.save()             
+
+def load(filename):
     db = AffiliationDB()
-    for to_match in load_json(filename):
-        c_match = match(db, candidate)
-        if c_match not in db: db.add(candidate, c_match)
+    load_into_db(filename, db)
     db.save()
 
     
